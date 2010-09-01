e1a849ea4c543ba9da00fd318bddcaf2e322a3da
leonhong <codeblocker@gmail.com>
2010-09-01 11:06:26 +0800

    add nginx conf

--- build.sh
+++ build.sh
@@ -1,2 +1,17 @@
 # !/bin/sh
-g++ fastcgi-app.cc engine.cc request.cc example.cc -L/home/hongliang/usr/lib -lfcgi++ -I/home/hongliang/usr/include/ -Wall -I. -I./c-ptr/ -I/home/hongliang/usr/boost-1.43.0/include/ -L /home/hongliang/usr/boost-1.43.0/lib/ -lboost_thread -o app
+#cgi-fcgi -start -connect /tmp/hello.sock ./app 
+
+#nignx
+#server {
+#	listen       8803;
+#	location / {
+#		root   html;
+#		#index  hello;
+#		#fastcgi_pass 192.168.6.7:9000;
+#		#fastcgi_index  example.fcgi;
+#		fastcgi_index  index.php;
+#		fastcgi_pass    unix:/tmp/hello.sock;
+#		include         /home/hongliang/usr/nginx/conf/fastcgi_params;
+#		}
+#}
+g++ fastcgi-app.cc engine.cc request.cc example.cc -L/home/hongliang/usr/lib -lfcgi++ -L/home/hongliang/usr/lib -lctemplate -pthread  -I/home/hongliang/usr/include/ -Wall -I. -I./c-ptr/ -I/home/hongliang/usr/boost-1.43.0/include/ -L /home/hongliang/usr/boost-1.43.0/lib/ -lboost_thread -o app
