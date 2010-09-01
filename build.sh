db485ab43217680967d4f54cec9605e022596063
leonhong <codeblocker@gmail.com>
2010-09-01 14:20:34 +0800

    app.cgi

--- build.sh
+++ build.sh
@@ -14,4 +14,13 @@
 #		include         /home/hongliang/usr/nginx/conf/fastcgi_params;
 #		}
 #}
-g++ fastcgi-app.cc engine.cc request.cc example.cc -L/home/hongliang/usr/lib -lfcgi++ -L/home/hongliang/usr/lib -lctemplate -pthread  -I/home/hongliang/usr/include/ -Wall -I. -I./c-ptr/ -I/home/hongliang/usr/boost-1.43.0/include/ -L /home/hongliang/usr/boost-1.43.0/lib/ -lboost_thread -o app
+
+#location ~ \.cgi$ {
+#	root   html;
+#	fastcgi_pass    unix:/tmp/hello.sock;
+#	fastcgi_index  app.cgi;
+#	fastcgi_param  SCRIPT_FILENAME  /home/hongliang/usr/nginx/html$fastcgi_script_name;
+#	include         /home/hongliang/usr/nginx/conf/fastcgi_params;
+#}
+
+g++ fastcgi-app.cc engine.cc request.cc example.cc -L/home/hongliang/usr/lib -lfcgi++ -L/home/hongliang/usr/lib -lctemplate -pthread  -I/home/hongliang/usr/include/ -Wall -I. -I./c-ptr/ -I/home/hongliang/usr/boost-1.43.0/include/ -L /home/hongliang/usr/boost-1.43.0/lib/ -lboost_thread -o app.cgi
