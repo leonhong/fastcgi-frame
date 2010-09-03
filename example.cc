324bb9aa9dc6797714f2374c8fdf40f1355343b5
leonhong <codeblocker@gmail.com>
2010-09-03 14:08:00 +0800

    add post

--- example.cc
+++ example.cc
@@ -23,6 +23,8 @@
 
 #include "fcgio.h"
 
+static const size_t STDIN_MAX = 1000000;
+
 static void tpl(std::ostream &out) {
 	ctemplate::TemplateDictionary dict("example");
 	dict.SetValue("NAME", "John Smith");
@@ -46,6 +48,53 @@ static void penv(std::ostream &out, char **envp) {
 	out << "</PRE>\n";
 }
 
+static size_t get_content(std::istream &in, std::ostream &out, std::ostream &err, char **envp, char **content) {
+	/*size_t len = 0;
+	char * clenstr = FCGX_GetParam("CONTENT_LENGTH", envp);
+	len = strtol(clenstr, &clenstr, 10);
+	std::string concent;
+	in >> concent;
+	out << concent;*/
+    
+	char * clenstr = FCGX_GetParam("CONTENT_LENGTH", envp);
+    size_t clen = STDIN_MAX;
+
+    if (clenstr)
+    {
+        clen = strtol(clenstr, &clenstr, 10);
+        if (*clenstr)
+        {
+            err << "can't parse \"CONTENT_LENGTH="
+                 << FCGX_GetParam("CONTENT_LENGTH", envp)
+                 << "\"\n";
+            clen = STDIN_MAX;
+        }
+
+        // *always* put a cap on the amount of data that will be read
+        if (clen > STDIN_MAX) clen = STDIN_MAX;
+
+        *content = new char[clen];
+
+        in.read(*content, clen);
+        clen = in.gcount();
+    }
+    else
+    {
+        // *never* read stdin when CONTENT_LENGTH is missing or unparsable
+        *content = 0;
+        clen = 0;
+    }
+
+    // Chew up any remaining stdin - this shouldn't be necessary
+    // but is because mod_fastcgi doesn't handle it correctly.
+
+    // ignore() doesn't set the eof bit in some versions of glibc++
+    // so use gcount() instead of eof()...
+    do in.ignore(1024); while (in.gcount() == 1024);
+
+    return clen;
+}
+
 class HelloWorld : public fastcgi::app {
 public:
 	fastcgi::app::status accept(std::istream &in, std::ostream &out, std::ostream &err, char **envp) {
@@ -54,15 +103,24 @@ public:
 				"\r\n";
 		/*out << "1Hello World Wide Web~!" << std::endl;
 		out << "Hello World Wide Web~!" << std::endl;*/
-		//std::cout << "Content-type: text/html\r\n" "r\n";
+		//out << "Content-type: text/html\r\n" "r\n";
 		tpl(out);
-		penv(out, envp);	
-		char *query_string;
+		//penv(out, envp);
+		
+		char *content = NULL;
+		size_t clen = get_content(in, out, err, envp, &content);
+        if (clen == STDIN_MAX) out << " (STDIN_MAX)";
+        out << " bytes</H4>\n";
+        if (clen) out.write(content, clen);
+        if (content) delete []content;
+		
+		/*char *query_string;
 		query_string = FCGX_GetParam("QUERY_STRING", envp);
 		if (query_string)
 			out << query_string << std::endl;
 		else
-			out << "query_string is null" << std::endl;
+			out << "query_string is null" << std::endl;*/
+		
 		return 0;
 	}
 };
